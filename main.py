ca0ff008105a087f8b81e6cf6169378e82f9f918
Roman <Tesmail@i.ua>
2024-01-14 22:32:46 +0200

    add poute photo_url_qr.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from src.routes import photo_url_qr
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+app.include_router(photo_url_qr.router, prefix='/api')
+
