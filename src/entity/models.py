d2e7d15ddf7f832851e96a720d6345928678fdc4
Bohdan Boiprav <122865615+bohdanboiprav@users.noreply.github.com>
2024-01-16 22:37:19 +0000

    Update models.py

--- src/entity/models.py
+++ src/entity/models.py
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-from datetime import date
-from sqlalchemy import String, types, ForeignKey, DateTime, func, Boolean
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.orm import DeclarativeBase
-import uuid
-=======
 import uuid
 from datetime import date
 from typing import List
@@ -14,14 +7,12 @@ from sqlalchemy import String, Date, func, DateTime, Enum, Integer, ForeignKey, 
 from sqlalchemy.orm import DeclarativeBase
 
 mapper_registry = registry()
->>>>>>> dev_new
 
 
 class Base(DeclarativeBase):
     pass
 
 
-<<<<<<< HEAD
 class User(Base):
     __tablename__ = 'users'
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
@@ -43,41 +34,6 @@ class UserType(Base):
     __tablename__ = 'user_type'
     id: Mapped[int] = mapped_column(primary_key=True)
     type: Mapped[str] = mapped_column(String(50), default=1)
-=======
-class UserType(Base):
-    __tablename__ = 'user_type'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user: Mapped[str] = mapped_column(String(50), nullable=False)
-
-
-class User(Base):
-    __tablename__ = 'users'
-    id: Mapped[uuid] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
-    email: Mapped[str] = mapped_column(String(150), unique=True, index=True, nullable=False)
-    password: Mapped[str] = mapped_column(String(500), nullable=False)
-    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
-    created_at: Mapped[date] = mapped_column('created_at', DateTime, default=func.now())
-    updated_at: Mapped[date] = mapped_column('updated_at', DateTime, default=func.now(), onupdate=func.now())
-    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
-    confirmed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
-    user_type_id: Mapped[int] = mapped_column(Integer, ForeignKey('user_type.id'), nullable=False)
-    user_type: Mapped["UserType"] = relationship("UserType", backref="users", lazy="joined")
-
-
-# association_table = Table(
-#     "tags_to_posts",
-#     Base.metadata,
-#     Column("post_id", ForeignKey("posts.id")),
-#     Column("tag_id", ForeignKey("tags.id")),
-# )
-
-# class Association(Base):
-#     __tablename__ = "tags_to_posts"
-#     # id: Mapped[int] = mapped_column(primary_key=True)
-#     post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), primary_key=True)
-#     tag_id: Mapped[int] = mapped_column(ForeignKey("tags.id"), primary_key=True)
-#     child: Mapped["Tag"] = relationship()
 
 
 class Post(Base):
@@ -90,9 +46,6 @@ class Post(Base):
     image: Mapped[str] = mapped_column(String(255), nullable=True)
     user_id: Mapped[uuid] = mapped_column(UUID(as_uuid=True), ForeignKey('users.id'), nullable=True)
     user: Mapped["User"] = relationship("User", backref="posts", lazy="joined")
-    # tags: Mapped[List["Association"]] = relationship(lazy="joined")
-    # tags: Mapped[List["Tag"]] = relationship(secondary=association_table)
-    # tags: Mapped[List["Tag"]] = relationship("TagToPost", backref="post", lazy="selectin")
 
     tags: Mapped[List["Tag"]] = relationship("Tag", secondary="tags_to_posts", back_populates="posts", lazy="joined")
     tags_to_posts: Mapped[List["TagToPost"]] = relationship("TagToPost", back_populates="post", lazy="joined",
@@ -109,7 +62,6 @@ class Tag(Base):
     __tablename__ = 'tags'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
-    # posts: Mapped[List["Post"]] = relationship("TagToPost", secondary='tags_to_posts', back_populates='posts')
 
     tags_to_posts: Mapped[List["TagToPost"]] = relationship("TagToPost", back_populates="tag", lazy="joined",
                                                             overlaps="posts,tags")
@@ -127,11 +79,3 @@ class TagToPost(Base):
 
 
 mapper_registry.configure()
-
-# class TagToPost(Base):
-#     __tablename__ = 'tags_to_posts'
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     post_id: Mapped[int] = mapped_column(Integer, ForeignKey('posts.id'), nullable=False)
-#     tag_id: Mapped[int] = mapped_column(Integer, ForeignKey('tags.id'), nullable=False)
-
->>>>>>> dev_new
