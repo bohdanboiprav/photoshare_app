9c0820b76cdd09cc6d7b61161cd181a6239439e2
F1r3n25 <dimalikegtr@gmail.com>
2024-01-18 23:48:49 +0200

    upd
    1. db/entity/models: add param to tags_to_post (67)
    2. add new Schema for DeletedPost
    3. add function-router "remove_tag"
    4. add forbidden in add_tags_to_post
    5. edit get_tag in repository.tags
    6. add remove_tag in repository.tags
    7. edit update_post
    8. edit add_tag_to_post
    9. edit remove_post

--- src/routes/tags.py
+++ src/routes/tags.py
@@ -2,8 +2,10 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Depends, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.db import get_db
+from src.entity.models import User
 from src.repository import tags as repository_tags
 from src.schemas.tag import TagResponse, TagModel, TagUpdate
+from src.services.auth import auth_service
 
 router = APIRouter(prefix="/tags", tags=["tags"])
 
@@ -12,7 +14,9 @@ router = APIRouter(prefix="/tags", tags=["tags"])
 async def create_tag(
         body: TagModel,
         db: AsyncSession = Depends(get_db),
+
 ):
+
     return await repository_tags.create_tag(body, db)
 
 
@@ -27,3 +31,14 @@ async def get_all_tags(limit: int = Query(10, ge=10, le=500), offset: int = Quer
 async def get_or_create_tag_by_name(name: str, db: AsyncSession = Depends(get_db)):
     tag = await repository_tags.get_or_create_tag_by_name(name, db)
     return tag
+
+
+@router.delete("/{tag_name}", response_model=TagResponse)
+async def remove_tag(tag_name: str, user: User = Depends(auth_service.get_current_user), db: AsyncSession = Depends(get_db)):
+    if user.user_type_id == 1:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin/moder can remove tags")
+    tag = await repository_tags.get_tag(tag_name, db)
+    if not tag:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag is not found")
+    removed_tag = await repository_tags.remove_tag(tag, db)
+    return removed_tag
