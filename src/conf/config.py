04ca61e78c8dd5edf4c8d07c8f12f036a19e27f5
Bohdan Boiprav <122865615+bohdanboiprav@users.noreply.github.com>
2024-01-23 12:53:48 +0000

    Update config.py

--- src/conf/config.py
+++ src/conf/config.py
@@ -3,23 +3,23 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = "postgresql+asyncpg://postgres:123456@localhost:5432/base"
-    SECRET_KEY_JWT: str = "aego6U6wk"
-    ALGORITHM: str = "HS256"
-    MAIL_USERNAME: str = "admin@example.com"
-    MAIL_PASSWORD: str = "password"
-    MAIL_FROM: str = "admin@example.com"
-    MAIL_PORT: int = 465
-    MAIL_SERVER: str = "smtp.example.com"
-    MAIL_USE_SSL: bool = True
-    REDIS_HOST: str = "localhost"
-    REDIS_PORT: int = 6379
+    SQLALCHEMY_DATABASE_URL: str
+    SECRET_KEY_JWT: str
+    ALGORITHM: str
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_USE_SSL: bool
+    REDIS_HOST: str
+    REDIS_PORT: int
     REDIS_PASSWORD: str | None = None
-    CLOUDINARY_NAME: str = "cloud_name"
-    CLOUDINARY_API_KEY: int = 123456789
-    CLOUDINARY_API_SECRET: str = "secret"
-    APP_ENV: str = "str"
-    ADMIN_PASSWORD: str = "str"
+    CLOUDINARY_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
+    APP_ENV: str
+    ADMIN_PASSWORD: str
 
     model_config = ConfigDict(extra='ignore', env_file=".env", env_file_encoding="utf-8")  # noqa
 
